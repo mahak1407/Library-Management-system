2198021a35e9a39de374c243d558653e3f68044c
Mahak <sainimahak1407@gmail.com>
2025-08-10 15:31:37 +0530

    Add initial data insertion to create MongoDB database

--- config/db.js
+++ config/db.js
@@ -1,12 +1,28 @@
-// connect to MongoDB database
-const mongoose = require('mongoose')
+// // connect to MongoDB database
+// const mongoose = require('mongoose')
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect('mongodb+srv://mahak:1407@cluster0.vauszcs.mongodb.net/')
+//         console.log('Connected to MongoDB sucessfully')
+//     }
+//     catch (err) {
+//         console.log('Error Occured:', err)
+//     };
+// }
+// module.exports = connectDB;
+
+const mongoose = require('mongoose');
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/Library')
-        console.log('Connected to MongoDB sucessfully')
-    }
-    catch (err) {
-        console.log('Error Occured:', err)
-    };
-}
+  try {
+    await mongoose.connect('mongodb+srv://mahak:1407@cluster0.vauszcs.mongodb.net/librarydb', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB successfully');
+  } catch (err) {
+    console.log('Error Occurred:', err);
+  }
+};
+
 module.exports = connectDB;
